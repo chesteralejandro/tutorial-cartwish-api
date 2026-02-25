67287d0730457196945a5f1191ef9d8160946330
Chester Alejandro <chesterm.alejandro@gmail.com>
2026-02-25 18:30:20 +0800

    feat: add user model

---
+++ src/models/user.model.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const USER_VALIDATION = {
+	name: { type: String, required: true, mingLength: 3 },
+	email: { type: String, required: true, unique: true, lowercase: true },
+	password: { type: String, required: false },
+	googleId: { type: String, unique: true },
+	deliveryAddress: { type: String, required: false },
+	role: { type: String, enum: ['user', 'admin'], default: 'user' },
+};
+
+const USER_COLLECTION_NAME = 'User';
+const USER_SCHEMA = new mongoose.Schema(USER_VALIDATION);
+
+const User = mongoose.model(USER_COLLECTION_NAME, USER_SCHEMA);
+
+module.exports = User;
