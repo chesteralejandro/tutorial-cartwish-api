1f8ac463a755c1d5b935fa4f034b75087735cb8a
Chester Alejandro <chesterm.alejandro@gmail.com>
2026-03-11 18:51:41 +0800

    feat: implement joi validation

--- src/modules/user/user.controller.js
+++ src/modules/user/user.controller.js
@@ -1,12 +1,28 @@
+const Joi = require('joi');
+
 const User = require('./user.model.js');
 const STATUS_CODES = require('../../constants/statusCodes.js');
 const { createHashedPassword } = require('../../utils/bcrypt.js');
+
+const createUserSchema = Joi.object({
+	name: Joi.string().min(3).required(),
+	email: Joi.string().email().required(),
+	password: Joi.string().min(6).required(),
+	deliveryAddress: Joi.string().min(5).required(),
+});
 
 class UserController {
 	async create(req, res) {
 		const userData = req.body;
 		try {
 			const userFound = await User.findOne({ email: userData.email });
+
+			const joiValidation = createUserSchema.validate(req.body);
+
+			if (joiValidation.error) {
+				res.status(400).json(joiValidation.error.details[0].message);
+				return;
+			}
 
 			if (userFound) {
 				throw new Error('User already exist.');
