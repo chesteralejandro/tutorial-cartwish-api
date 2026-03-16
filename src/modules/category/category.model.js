bfcd71dc2efa3fc43ea36934a969862d7aa79250
Chester Alejandro <chesterm.alejandro@gmail.com>
2026-03-16 12:39:27 +0800

    feat: add category model

---
+++ src/modules/category/category.model.js
@@ -0,0 +1,13 @@
+const mongoose = requrie('mongoose');
+
+const CATEGORY_VALIDATION = {
+	name: { type: String, required: true, unique: true },
+	image: { type: String, required: true },
+};
+
+const CATEGORY_COLLECTION_NAME = 'Category';
+const CATEGORY_SCHEMA = new mongoose.Schema(CATEGORY_VALIDATION);
+
+const Category = mongoose.model(CATEGORY_COLLECTION_NAME, CATEGORY_SCHEMA);
+
+module.exports = Category;
