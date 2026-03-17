78a0c9a65f962398f5f64dbe37bcd578f14488e2
Chester Alejandro <chesterm.alejandro@gmail.com>
2026-03-17 23:22:29 +0800

    fix: checkRole condition

--- src/middlewares/checkRole.middleware.js
+++ src/middlewares/checkRole.middleware.js
@@ -2,7 +2,8 @@ const { STATUS_CODES } = require('../config/constants');
 
 const checkRole = (role) => {
 	return (req, res, next) => {
-		if (!req.user || !req.user.role !== role) {
+		if (!req.user || req.user.role !== role) {
+			console.log(req.user.role);
 			res.status(STATUS_CODES.FORBIDDEN).json({
 				message: `Access denied: ${role} only!`,
 			});
