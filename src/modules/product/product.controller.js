394b6357bd92d1d89147a6f2d0c6a363cbadcd74
Chester Alejandro <chesterm.alejandro@gmail.com>
2026-03-23 13:45:29 +0800

    refactor: add product controller

---
+++ src/modules/product/product.controller.js
@@ -0,0 +1,179 @@
+const fs = require('node:fs/promises');
+const path = require('node:path');
+
+const Product = require('./product.model');
+const Category = require('../category/category.model');
+
+const { STATUS_CODES } = require('../../config/constants');
+
+class ProductController {
+	async create(req, res) {
+		const { title, description, category, price, stock } = req.body;
+		const images = req.files.map((image) => image.filename);
+
+		if (images.length === 0) {
+			res.status(STATUS_CODES.BAD_REQUEST).json({
+				message: 'At least one image is required!',
+			});
+
+			return;
+		}
+
+		const newProduct = new Product({
+			title,
+			description,
+			category,
+			price,
+			stock,
+			images,
+			seller: req.user._id,
+		});
+
+		await newProduct.save();
+
+		res.status(STATUS_CODES.CREATED).json(newProduct);
+	}
+
+	async getAll(req, res) {
+		const SINGLE_PAGE = 1;
+		const ITEMS_PER_PAGE = 8;
+		const INITIAL_RATING = 0;
+		const ANTI_NAN_VALUE = 1;
+
+		const page = parseInt(req.query.page) || SINGLE_PAGE;
+		const perPage = parseInt(req.query.perPage) || ITEMS_PER_PAGE;
+		const category = req.query.category;
+		const search = req.query.search;
+
+		const query = {};
+
+		if (category) {
+			const categoryFound = await Category.findOne({ name: category });
+
+			if (!categoryFound) {
+				res.status(STATUS_CODES.NOT_FOUND).json({
+					message: 'Category not found!',
+				});
+
+				return;
+			}
+
+			query.category = categoryFound._id;
+		}
+
+		if (search) {
+			query.title = { $regex: search, $options: 'i' };
+		}
+
+		const products = await Product.find(query)
+			.select('-description -seller -category -__v')
+			.skip((page - SINGLE_PAGE) * perPage)
+			.limit(perPage)
+			.lean(); // Returns clean object from mongoose without extra properties.
+
+		const updatedProducts = products.map((product) => {
+			const numberOfReviews = product.reviews.length;
+			const sumOfRatings = product.reviews.reduce(
+				(sum, review) => sum + review.rating,
+				INITIAL_RATING,
+			);
+			const averageRating =
+				sumOfRatings / (numberOfReviews || ANTI_NAN_VALUE);
+
+			return {
+				...product,
+				images: product.images[0],
+				reviews: { numberOfReviews, averageRating },
+			};
+		});
+
+		const totalProducts = await Product.countDocuments(query);
+		const totalPages = Math.ceil(totalProducts / perPage);
+
+		res.status(STATUS_CODES.OK).json({
+			updatedProducts,
+			totalProducts,
+			totalPages,
+			currentPage: page,
+			postPerPage: perPage,
+		});
+	}
+
+	async getOne(req, res) {
+		const productId = req.params.productId;
+
+		const productFound = await Product.findById(productId)
+			.populate('seller', '_id name email')
+			.populate('reviews.user', '_id name email')
+			.select('-category -__v');
+
+		if (!productFound) {
+			res.status(STATUS_CODES.NOT_FOUND).json({
+				message: 'Product not found!',
+			});
+
+			return;
+		}
+
+		res.status(STATUS_CODES.OK).json(productFound);
+	}
+
+	async delete(req, res) {
+		const productId = req.params.productId;
+
+		const productFound =
+			await Product.findById(productId).select('seller images');
+
+		if (!productFound) {
+			res.status(STATUS_CODES.NOT_FOUND).json({
+				message: 'Product not found!',
+			});
+
+			return;
+		}
+
+		if (
+			req.user.role === 'admin' ||
+			req.user._id.toString() === productFound.seller.toString()
+		) {
+			await productFound.deleteOne();
+
+			if (productFound.images && Boolean(productFound.images.length)) {
+				productFound.images.forEach(async (imageName) => {
+					const fullPath = path.join(
+						__dirname,
+						'..',
+						'..',
+						'..',
+						'upload',
+						'products',
+						imageName,
+					);
+
+					try {
+						await fs.unlink(fullPath);
+					} catch (error) {
+						console.error(
+							'Error deleting file:',
+							fullPath,
+							error.message,
+						);
+					}
+				});
+			}
+
+			res.status(STATUS_CODES.OK).json({
+				message: 'Product deleted successfully!',
+			});
+
+			return;
+		}
+
+		res.status(STATUS_CODES.FORBIDDEN).json({
+			message:
+				'Access denied: Only admin or seller can delete this product!',
+		});
+	}
+}
+
+module.exports = new ProductController();
--- src/modules/product/product.routes.js
+++ src/modules/product/product.routes.js
@@ -1,185 +1,23 @@
-const fs = require('node:fs/promises');
-const path = require('node:path');
 const router = require('express').Router();
+
+const productController = require('./product.controller');
 
 const authMiddleware = require('../../middlewares/auth.middleware');
 const checkRole = require('../../middlewares/checkRole.middleware');
 const handleUpload = require('../../middlewares/upload.middleware');
-const { STATUS_CODES } = require('../../config/constants');
-const Product = require('./product.model');
-const Category = require('../category/category.model');
 
 router.post(
 	'/',
 	authMiddleware,
 	checkRole('seller'),
 	handleUpload('array', 'images', 'products'),
-	async (req, res) => {
-		const { title, description, category, price, stock } = req.body;
-		const images = req.files.map((image) => image.filename);
-
-		if (images.length === 0) {
-			res.status(STATUS_CODES.BAD_REQUEST).json({
-				message: 'At least one image is required!',
-			});
-
-			return;
-		}
-
-		const newProduct = new Product({
-			title,
-			description,
-			category,
-			price,
-			stock,
-			images,
-			seller: req.user._id,
-		});
-
-		await newProduct.save();
-
-		res.status(STATUS_CODES.CREATED).json(newProduct);
-	},
+	productController.create,
 );
 
-router.get('/', async (req, res) => {
-	const SINGLE_PAGE = 1;
-	const ITEMS_PER_PAGE = 8;
-	const INITIAL_RATING = 0;
-	const ANTI_NAN_VALUE = 1;
+router.get('/', productController.getAll);
 
-	const page = parseInt(req.query.page) || SINGLE_PAGE;
-	const perPage = parseInt(req.query.perPage) || ITEMS_PER_PAGE;
-	const category = req.query.category;
-	const search = req.query.search;
+router.get('/:productId', productController.getOne);
 
-	const query = {};
-
-	if (category) {
-		const categoryFound = await Category.findOne({ name: category });
-
-		if (!categoryFound) {
-			res.status(STATUS_CODES.NOT_FOUND).json({
-				message: 'Category not found!',
-			});
-
-			return;
-		}
-
-		query.category = categoryFound._id;
-	}
-
-	if (search) {
-		query.title = { $regex: search, $options: 'i' };
-	}
-
-	const products = await Product.find(query)
-		.select('-description -seller -category -__v')
-		.skip((page - SINGLE_PAGE) * perPage)
-		.limit(perPage)
-		.lean(); // Returns clean object from mongoose without extra properties.
-
-	const updatedProducts = products.map((product) => {
-		const numberOfReviews = product.reviews.length;
-		const sumOfRatings = product.reviews.reduce(
-			(sum, review) => sum + review.rating,
-			INITIAL_RATING,
-		);
-		const averageRating =
-			sumOfRatings / (numberOfReviews || ANTI_NAN_VALUE);
-
-		return {
-			...product,
-			images: product.images[0],
-			reviews: { numberOfReviews, averageRating },
-		};
-	});
-
-	const totalProducts = await Product.countDocuments(query);
-	const totalPages = Math.ceil(totalProducts / perPage);
-
-	res.status(STATUS_CODES.OK).json({
-		updatedProducts,
-		totalProducts,
-		totalPages,
-		currentPage: page,
-		postPerPage: perPage,
-	});
-});
-
-router.get('/:productId', async (req, res) => {
-	const productId = req.params.productId;
-
-	const productFound = await Product.findById(productId)
-		.populate('seller', '_id name email')
-		.populate('reviews.user', '_id name email')
-		.select('-category -__v');
-
-	if (!productFound) {
-		res.status(STATUS_CODES.NOT_FOUND).json({
-			message: 'Product not found!',
-		});
-
-		return;
-	}
-
-	res.status(STATUS_CODES.OK).json(productFound);
-});
-
-router.delete('/:productId', authMiddleware, async (req, res) => {
-	const productId = req.params.productId;
-
-	const productFound =
-		await Product.findById(productId).select('seller images');
-
-	if (!productFound) {
-		res.status(STATUS_CODES.NOT_FOUND).json({
-			message: 'Product not found!',
-		});
-
-		return;
-	}
-
-	if (
-		req.user.role === 'admin' ||
-		req.user._id.toString() === productFound.seller.toString()
-	) {
-		await productFound.deleteOne();
-
-		if (productFound.images && Boolean(productFound.images.length)) {
-			productFound.images.forEach(async (imageName) => {
-				const fullPath = path.join(
-					__dirname,
-					'..',
-					'..',
-					'..',
-					'upload',
-					'products',
-					imageName,
-				);
-
-				try {
-					await fs.unlink(fullPath);
-				} catch (error) {
-					console.error(
-						'Error deleting file:',
-						fullPath,
-						error.message,
-					);
-				}
-			});
-		}
-
-		res.status(STATUS_CODES.OK).json({
-			message: 'Product deleted successfully!',
-		});
-
-		return;
-	}
-
-	res.status(STATUS_CODES.FORBIDDEN).json({
-		message: 'Access denied: Only admin or seller can delete this product!',
-	});
-});
+router.delete('/:productId', authMiddleware, productController.delete);
 
 module.exports = router;
