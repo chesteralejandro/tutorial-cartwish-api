dfd0d208533154746f4514822d46889b9e920643
Chester Alejandro <chesterm.alejandro@gmail.com>
2026-03-23 14:44:54 +0800

    style: reorder user routes require and remove console log

--- src/middleware/handleRole.js
+++ src/middleware/handleRole.js
@@ -3,7 +3,6 @@ const { STATUS_CODES } = require('../config/constants');
 const handleRole = (role) => {
 	return (req, res, next) => {
 		if (!req.user || req.user.role !== role) {
-			console.log(req.user.role);
 			res.status(STATUS_CODES.FORBIDDEN).json({
 				message: `Access denied: ${role} only!`,
 			});
--- src/modules/user/user.routes.js
+++ src/modules/user/user.routes.js
@@ -1,7 +1,8 @@
 const router = require('express').Router();
-const handleAuth = require('../../middleware/handleAuth.js');
 
 const userController = require('./user.controller.js');
+
+const handleAuth = require('../../middleware/handleAuth.js');
 
 router.get('/', handleAuth, userController.showDashboard);
 
